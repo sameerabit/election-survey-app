fba17ab626154af4a3039f0e39437acdb0795e3a
Viraj Amarasinghe <virajamara@gmail.com>
2024-09-14 05:27:03 +0200

    email added

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -28,7 +28,7 @@ const Footer: React.FC = () => {
             safeguarding your information. If you
             have any questions or concerns about
             your data, please feel free to contact
-            us via my email virajamara@gmail.com
+            me via email virajamara@gmail.com
           </p>
         </div>
       </div>
